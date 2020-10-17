21509a0922647f7a6757f9c03c462402e4af26dc
Matthew Tsai <mtsai1@berkeley.edu>
2020-10-16 20:56:25 -0700

    Revised Resume and added more pop ups

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function myPopUp() {
-    var popup = document.getElementById("myPopup");
+function myPopUp(str1) {
+    var popup = document.getElementById(str1);
     popup.classList.toggle("show");
   }
